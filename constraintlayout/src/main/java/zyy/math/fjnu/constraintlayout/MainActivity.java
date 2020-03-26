c0df6966d9125cdf7fe733516a72b23aedbd667c
zhanyaoyu <742857154@qq.com>
2020-03-26 16:59:16 +0800

    第一次提交

---
+++ constraintlayout/src/main/java/zyy/math/fjnu/constraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package zyy.math.fjnu.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
